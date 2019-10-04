239198dd582c213f1bdb8ab8b462b257281d8592
ugogo <hello@onali-ugo.fr>
2019-10-04 16:47:45 +0200

    up

--- install.sh
+++ install.sh
@@ -6,12 +6,15 @@ brew install autojump
 brew install wget
 brew install tmate
 brew install yarn
-brew tap caskroom/fonts
-brew cask install font-fira-code
+brew tap caskroom/fonts && brew cask install font-fira-code
 brew cask install ngrok
+brew install nvm
 
 # install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # move files
 cp .gitignore_global ~/
