04e63c0d46e8acc234a192012f7de932d4c00a8e
ugogo <ugo.j.onali@gmail.com>
2025-08-19 22:49:33 +0200

    update

--- install.sh
+++ install.sh
@@ -1,33 +1,68 @@
 # install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+if [ -n $ZSH ]; then
+  echo "$ Zsh already installed, skipping..."
+else
+  echo "$ Installing zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 # install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if [[ $(command -v brew) == "" ]]; then
+    echo "$ Installing Hombrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "$ Updating Homebrew"
+    brew update
+fi
 
 # brew packages
-brew tap homebrew/cask-fonts
-brew install autojump diff-so-fancy fnm git-recent postgres whatsapp wget yarn
-brew install --cask 1password arc bartender cleanshot cron deepl dropbox font-fira-code flux iterm2 linear-linear ngrok raycast slack spotify visual-studio-code vlc
+ echo "$ Installing brew packages..."
+ brew tap homebrew/cask-fonts
+ brew install \
+ autojump \
+ diff-so-fancy \
+ fnm \
+ git-recent \
+ pure \
+ whatsapp \
+ wget \
+ yarn
+brew install --cask \
+1password \
+arc \
+bartender \
+cleanshot \
+cursor \
+cron \
+deepl \
+dropbox \
+font-fira-code \
+flux \
+iterm2 \
+linear-linear \
+ngrok \
+raycast \
+slack \
+spotify \
+vlc
 
 # zsh plugins
+echo "$ Installing zsh plugins..."
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
 
-# npm globals
-npm install --global pure-prompt
+# npm packages
+echo "$ Installing npm packages..."
 npm install --global git-open
+npm install --global npm-check-updates
 
 # move files
+echo "$ Moving files..."
 cp .gitignore_global ~/
 cp .gitconfig ~/
 cp .zshrc ~/
 
-# open apps
+# # open apps
 open /Applications/Flux.app
 open /Applications/Dropbox.app/
 
-# postgres
-# https://gist.github.com/ibraheem4/ce5ccd3e4d7a65589ce84f2a3b7c23a3
-# ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
-# createdb `whoami`
-# brew services start postgresql
